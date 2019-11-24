0750033ba0b2d6c7fdc72d90647b6582d2e49de3
Jiri Hubacek <hubacekjirka@gmail.com>
2019-11-24 13:01:48 +1100

    version 0,1

--- Nerdy Dice/ViewController.swift
+++ Nerdy Dice/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let diceOptions = [#imageLiteral(resourceName: "1"),#imageLiteral(resourceName: "2"),#imageLiteral(resourceName: "3"),#imageLiteral(resourceName: "4"),#imageLiteral(resourceName: "5"),#imageLiteral(resourceName: "6")]
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        let toImage = self.diceOptions.randomElement()
+        UIView.transition(with: self.diceImageView,
+                          duration: 0.3,
+                          options: .transitionCrossDissolve,
+                          animations: {
+                            self.diceImageView.image = toImage
+        },
+                          completion: nil)
+    }
+    
+    @IBOutlet weak var diceImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("started")
     }
-
-
 }
 
